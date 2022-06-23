ab4f52e1470a576a4d7ac1d0fdfbb956f37cb99d
Sparsh003 <sparsh.dev3@gmail.com>
2022-06-23 21:33:54 +0530

    React redux configuration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import CardDetail from "./components/CardDetail";
+import NavBar from "./Layout/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Router>
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Dashboard />} />
+            <Route exact path="/post/:id" element={<CardDetail />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
